1ac308d65e9008fa94c7c651ef6cd427aee5ab50
ugolole <ugololeivan@yahoo.com>
2019-03-11 14:32:08 -0400

    adding new quiz component file

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { QuizListComponent } from './quiz/quiz-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    QuizListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- ClientApp/src/app/interfaces/quiz.ts
+++ ClientApp/src/app/interfaces/quiz.ts
@@ -1,1 +1,1 @@
-interface Quiz {     Id : number,     Title : string,     Description : string,     Text : string } 
+interface Quiz {     Id : number,     Title : string,     Description : string,     Text : string }  
