c74f36c8b9fba51fdde569343e9b528d005e866c
ugolole <ugololeivan@yahoo.com>
2019-03-12 13:42:57 -0400

    adding quiz component to the quiz folder

---
+++ ClientApp/src/app/quiz/quiz.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: "quiz",
+    templateUrl : "./quiz.component.html",
+    styleUrls : ['./quiz.component.css']
+})
+
+export class QuizComponent {
+    @Input() quiz : Quiz;
+}
