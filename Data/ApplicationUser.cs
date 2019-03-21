334029ebf12ba4e2b275d85636fc4006ec62041d
ugolole <ugololeivan@yahoo.com>
2019-03-20 23:33:39 -0400

    adding data model and all the needed files with all the code

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestApplication.Data
+{
+    public class ApplicationUser
+    {
+        #region constructor
+        public ApplicationUser()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+
+        [Required]
+        [MaxLength(128)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Notes { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+        [Required]
+        public int Flags { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+        #endregion
+
+        #region Lazy-load Properties
+        ///<summary>
+        ///A list of all quiz created by this users
+        ///</summary>
+        public virtual List<Quiz> Quizzes { get; set; }
+        #endregion
+    }
+}
