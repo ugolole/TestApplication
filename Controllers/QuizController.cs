8ce8338dff0c1d2291ae8641bc768034ca70e1f1
ugolole <ugololeivan@yahoo.com>
2019-03-24 20:15:08 -0400

    adding new methods to add delete and update the quiz

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -40,10 +40,135 @@ namespace TestApplication.Controllers
         {
             var quiz = DbContext.Quizzes.Where(i => i.Id == id).FirstOrDefault();
 
+            //handle request asking for non-existing quizzes 
+            if (quiz == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Quiz ID {0} has not been found", id)
+                });
+            }
+
             return new JsonResult(quiz.Adapt<QuizViewModel>(), new JsonSerializerSettings()
             {
                 Formatting = Formatting.Indented
             });
+        }
+
+        /// <summary>
+        /// Adds new quiz to the database
+        /// </summary>
+        /// <param name="model">Teh QuizViewModel containing the data to insert</param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Put([FromBody]QuizViewModel model)
+        {
+            //return a generic HTTP Status 500 (Server Error)
+            //if the client payload is invalid
+            if (model == null) return new StatusCodeResult(500);
+
+            //handle the insert (without object-mapping).
+            var quiz = new Quiz();
+
+            //properties taken from the request
+            quiz.Title = model.Title;
+            quiz.Description = model.Description;
+            quiz.Text = model.Text;
+            quiz.Notes = model.Notes;
+
+            //properties set from the server side
+            quiz.CreatedDate = DateTime.Now;
+            quiz.LastModifiedDate = quiz.CreatedDate;
+
+            //set a temporary author using the Admin user's userId
+            //as user login is not supported yet:
+            quiz.UserId = DbContext.Users.Where(u => u.UserName == "Admin").FirstOrDefault().Id;
+
+            // add the new quiz
+            DbContext.Quizzes.Add(quiz);
+            //persist the changes into the Database.
+            DbContext.SaveChanges();
+
+            //return the newly created quiz to the client
+            return new JsonResult(quiz.Adapt<QuizViewModel>(), new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            });
+        }
+
+        /// <summary>
+        /// Edit the quiz with given {id}
+        /// </summary>
+        /// <param name="model">The QuizViewModel containing the data to update</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post([FromBody]QuizViewModel model)
+        {
+            //return generic HTTP Status 500 (Server Error)
+            //if the client payload is invalid
+            if (model == null) return new StatusCodeResult(500);
+
+            //retrieve the quiz to edit
+            var quiz = DbContext.Quizzes.Where(q => q.Id == model.Id).FirstOrDefault();
+
+            //handle requests asking for non-existing quizzes 
+            if (quiz == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Quiz ID {0} has not been found", model.Id)
+                });
+            }
+
+            //handle the update (without object - mapping)
+            // by manually assigning properties
+            //we want to accept form the request
+            quiz.Title = model.Title;
+            quiz.Description = model.Description;
+            quiz.Text = model.Text;
+            quiz.Notes = model.Notes;
+
+            //properties set from server-side
+            quiz.LastModifiedDate = quiz.CreatedDate;
+
+            //persist the changes to the database
+            DbContext.SaveChanges();
+
+            //return the update Quiz to the client
+            return new JsonResult(quiz.Adapt<QuizViewModel>(), new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            });
+        }
+
+        /// <summary>
+        /// Delete the quiz with given {ID} from the database
+        /// </summary>
+        /// <param name="id">The Id of the existing test</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //retrieve the quiz from the database
+            var quiz = DbContext.Quizzes.Where(i => i.Id == id).FirstOrDefault();
+
+            //handle request asking for non-existing quizzes
+            if(quiz == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Quiz ID {0} has not been found", id)
+                });
+            }
+
+            //remove teh quiz from the DbContext
+            DbContext.Quizzes.Remove(quiz);
+
+            //persist the changes into the Databases
+            DbContext.SaveChanges();
+
+            //return an HTTP Status 200 (ok)
+            return new OkResult();
         }
 
         #region 
@@ -62,7 +187,7 @@ namespace TestApplication.Controllers
                 .OrderByDescending(q => q.CreatedDate)
                 .Take(num)
                 .ToArray();
-            return new JsonResult(latest.Adapt<QuizViewModel[]>(), 
+            return new JsonResult(latest.Adapt<QuizViewModel[]>(),
                 new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
@@ -82,7 +207,7 @@ namespace TestApplication.Controllers
                 .Take(num)
                 .ToArray();
 
-            return new JsonResult( byTitle.Adapt<QuizViewModel[]>(),
+            return new JsonResult(byTitle.Adapt<QuizViewModel[]>(),
                 new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
@@ -102,27 +227,11 @@ namespace TestApplication.Controllers
                 .ToArray();
 
             return new JsonResult(
-                random.Adapt<QuizViewModel[]>(), new JsonSerializerSettings() {
+                random.Adapt<QuizViewModel[]>(), new JsonSerializerSettings()
+                {
                     Formatting = Formatting.Indented
                 });
         }
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpPost]
-        public IActionResult Post(QuizViewModel m)
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpPut]
-        public IActionResult Put(QuizViewModel m)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
